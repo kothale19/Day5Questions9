d47b9fd4927fa4da0d9b23fc5f2f91dad096ed03
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-11 17:56:51 +0530

    Added: Alphabet Vowels/Consonant logic

--- src/com/blz/VowelsOrConsanant/checkVowelsOrConsanant.java
+++ src/com/blz/VowelsOrConsanant/checkVowelsOrConsanant.java
@@ -1,10 +1,33 @@
 package com.blz.VowelsOrConsanant;
-
+import java.util.Scanner;
 public class checkVowelsOrConsanant {
 
 	public static void main(String[] args) {
 	System.out.println("Welcome to check Alphabet is Vowels Or Cosanant Program!!");
-
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Enter any Alphabet:");
+     char Alphabet = scan.next().charAt(0);
+     
+        switch(Alphabet) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+        	System.out.println(Alphabet+ " is a Vowel");
+        	break;
+        default:
+        	System.out.println(Alphabet+ " is a Consanant");
+        }
+        	
+            
+     
+	    	
+	    }
+	    	
+	
+	
+	
 	}
 
-}
+
