4588e7c05c6d4362b4ea2fb37fd22eb3ff938543
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-11 16:41:53 +0530

    Added: Welcome message

---
+++ src/com/blz/VowelsOrConsanant/checkVowelsOrConsanant.java
@@ -0,0 +1,10 @@
+package com.blz.VowelsOrConsanant;
+
+public class checkVowelsOrConsanant {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to check Alphabet is Vowels Or Cosanant Program!!");
+
+	}
+
+}
