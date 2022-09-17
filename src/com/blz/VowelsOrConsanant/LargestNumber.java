5f1373c4484483ecb0ca69ee39eabd6d648ebf05
kothale19 <kothale.pujaprasad@gmail.com>
2022-09-17 15:40:51 +0530

    Added: Welcome To Largest Number Programgit add .

---
+++ src/com/blz/VowelsOrConsanant/LargestNumber.java
@@ -0,0 +1,10 @@
+package com.blz.VowelsOrConsanant;
+
+public class LargestNumber {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Largest Number Program!!");
+
+	}
+
+}
